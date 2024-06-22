db18c74ab369ead7ab271544120b0511ac63766e
otabekpanoev01 <otabekpanoev01@gmail.com>
2024-06-22 14:41:34 +0500

    db prepare test

---
+++ src/main/java/uz/pdp/TestJDBC.java
@@ -0,0 +1,72 @@
+package uz.pdp;
+
+import java.sql.*;
+
+public class TestJDBC {
+    public static void main(String[] args) {
+
+//        select();
+
+        insert();
+
+
+    }
+
+    private static void insert() {
+        try {
+            Connection con = getConnection();
+
+            String sql = "insert into accounts(name, balance ) values(?,?), (?, ?)";
+
+            PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, "Sarvar");
+            st.setDouble(2, 100.11);
+
+            st.setString(3, "G'olib");
+            st.setDouble(4, 200.1);
+
+            int res = st.executeUpdate();
+            System.out.println(res);
+
+            con.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Connection getConnection() throws Exception {
+
+        Connection con = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "root123");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return con;
+    }
+
+    private static void select() {
+        try {
+            Connection con = getConnection();
+
+            String sql = "select * from accounts";
+            PreparedStatement st = con.prepareStatement(sql);
+
+            ResultSet resultSet = st.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                double balance = resultSet.getDouble("balance");
+
+                System.out.println("id = " + id + " name = " + name + " balance = " + balance);
+            }
+
+
+            con.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
